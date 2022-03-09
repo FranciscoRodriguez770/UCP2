0fb573e73d9a7a6ad1ba6359fa118cb7021eb6de
FranciscoRodriguez770 <FranciscoRodriguez770@comunidadunir.net>
2022-03-09 09:14:50 +0000

    Terraform ok

--- Terraform/correcion-vars.tf
+++ Terraform/correcion-vars.tf
@@ -1,25 +1,23 @@
 variable "location" {
-  type = string
-  description = "Región de Azure donde crearemos la infraestructura"
-  default = "West Europe"
+  type         = string
+  description  = "Región de Azure donde crearemos la infraestructura"
+  default      = "West Europe"
 }
 
 variable "storage_account" {
-  type = string
-  description = "Nombre para la storage account"
-  default="staccountcp2fr770"
+  type         = string
+  description  = "Nombre para la storage account"
+  default      ="staccountcp2fr770"
 } 
 
 variable "public_key_path" {
-  type = string
-  description = "Ruta de la clave publica de acceso a las instancias"
-  default="c:/Users/frodrigue53m/.ssh/k8s.pub"
+  type         = string
+  description  = "Ruta de la clave publica de acceso a las instancias"
+  default      ="c:/Users/frodrigue53m/.ssh/k8s.pub"
 }
 
 variable "ssh_user" {
-  type = string
-  description="Usuario para hacer ssh"
-  default= "adminUsername"
+  type         = string
+  description  ="Usuario para hacer ssh"
+  default      = "adminUsername"
 }
-
- 
